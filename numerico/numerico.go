560025890407a339c16e051f0eff9abd2c049a77
david.urreta@gmail.com <david.urreta@gmail.com>
2018-05-13 19:35:13 +0200

    Algoritmos para factor común y encontrar números primos

--- numerico/numerico.go
+++ numerico/numerico.go
@@ -10,7 +10,6 @@ func Mcd(a, b int) int {
 	return a
 }
 
-
 func factn(b int, n int) (pot int, s int) {
 	m := b
 	s = 1
@@ -24,8 +23,8 @@ func factn(b int, n int) (pot int, s int) {
 	return
 }
 
-// exponente. a elevado a b 
-func Exponente(a float64, b int)  float64 {
+// exponente. a elevado a b
+func Exponente(a float64, b int) float64 {
 
 	var pa, final float64
 	final = 1
@@ -33,8 +32,8 @@ func Exponente(a float64, b int)  float64 {
 
 		dos, mul := factn(b, 2)
 		pa = a
-		for x:=0; x < dos; x++ {
-			pa = pa*pa
+		for x := 0; x < dos; x++ {
+			pa = pa * pa
 		}
 		final *= pa
 
@@ -46,3 +45,53 @@ func Exponente(a float64, b int)  float64 {
 	return final
 }
 
+func factorprimo(numero int) [][2]int {
+	i := 2
+	res := make([][2]int, 0, 3)
+	var fact, times int
+	for i < numero {
+		for numero%i == 0 {
+			fact = i
+			times++
+			numero = numero / i
+		}
+		if times > 0 {
+			res = append(res, [2]int{fact, times})
+		}
+		fact, times = 0, 0
+		i++
+	}
+	if numero > 1 {
+		res = append(res, [2]int{numero, 1})
+	}
+	return res
+}
+
+//Criba de eratostenes
+//Si el valor del slice es true entonces el número correspondiente al indice es primo.
+func findPrimos(limit int) []bool {
+
+	res := make([]bool, limit, limit)
+
+	//Inicializamos el array a true, por defecto está a false
+	//Todos los números pares no son primos.
+	for i := range res {
+		if i > 2 && i%2 != 0 {
+			res[i] = true
+		} else {
+			res[i] = false
+		}
+	}
+
+	//No hace falta tratar los números pares
+	for x := 3; x < limit; x = x + 2 {
+		//La criba empieza en el número siguiente
+		for y := x + 1; y < limit; y++ {
+			if y%x == 0 {
+				res[y] = false
+			}
+		}
+
+	}
+	return res
+}
--- numerico/numerico_test.go
+++ numerico/numerico_test.go
@@ -4,49 +4,48 @@ import "testing"
 
 func TestMcd(t *testing.T) {
 
-	flagtest := []struct{
-		a int
-		b int
+	flagtest := []struct {
+		a  int
+		b  int
 		re int
 	}{
-		{34,7,1},
-		{54,12,6},
-		{56,42,14},
+		{34, 7, 1},
+		{54, 12, 6},
+		{56, 42, 14},
 		{86329, 131, 131},
-		{13,13,13},
-		{273, 182,91},
-
+		{13, 13, 13},
+		{273, 182, 91},
 	}
 
-	for _,fl := range flagtest {
-	mc := Mcd(fl.a, fl.b)
+	for _, fl := range flagtest {
+		mc := Mcd(fl.a, fl.b)
 
-	if mc != fl.re {
-		t.Errorf("Expected %d | Actual %d ",fl.re, mc)
+		if mc != fl.re {
+			t.Errorf("Expected %d | Actual %d ", fl.re, mc)
+		}
 	}
-}
 
 }
 
 func TestExponente(t *testing.T) {
-	flagtest := []struct{
-		a float64
-		b int
+	flagtest := []struct {
+		a  float64
+		b  int
 		re float64
 	}{
-			{3,1,3},
-			{7,3, 343},
-			{4,4,256},
-			{3,7,2187},
-			{7,6,117649},
-			{3,40,12157665459056928801},
+		{3, 1, 3},
+		{7, 3, 343},
+		{4, 4, 256},
+		{3, 7, 2187},
+		{7, 6, 117649},
+		{3, 40, 12157665459056928801},
 	}
 
 	var pot float64
 	for _, fl := range flagtest {
 		pot = Exponente(fl.a, fl.b)
-		if (pot != fl.re) {
-			t.Errorf("Values:%f, %d | Expoted:%f, Actual:%f",fl.a, fl.b, fl.re, pot)
+		if pot != fl.re {
+			t.Errorf("Values:%f, %d | Expoted:%f, Actual:%f", fl.a, fl.b, fl.re, pot)
 		}
 	}
 }
@@ -76,6 +75,68 @@ func TestFactn(t *testing.T) {
 		}
 		if val != fl.re2 {
 			t.Errorf("Error total. Values:%d | Expoted:%d, Actual:%d", fl.b, fl.re2, val)
+		}
+	}
+}
+
+type resFaPrimo [][2]int
+
+func TestFactorPrimo(t *testing.T) {
+	flagtest := []struct {
+		val int
+		res resFaPrimo
+	}{
+		{
+			10,
+			resFaPrimo{{2, 1}, {5, 1}},
+		},
+		{
+			11,
+			resFaPrimo{{11, 1}},
+		},
+		{
+			123,
+			resFaPrimo{{3, 1}, {41, 1}},
+		},
+		{
+			256,
+			resFaPrimo{{2, 8}},
+		},
+		{
+			234,
+			resFaPrimo{{2, 1}, {3, 2}, {13, 1}},
+		},
+	}
+	var sol resFaPrimo
+	for _, fp := range flagtest {
+		sol = factorprimo(fp.val)
+		for pos, r1 := range fp.res {
+			if r1[0] != sol[pos][0] || r1[1] != sol[pos][1] {
+				t.Errorf("Error, Values:%d | Expected:%v, Actual:%v", fp.val, fp.res, sol)
+			}
+		}
+	}
+}
+
+func TestFindPrimos(t *testing.T) {
+
+	firstprim := []bool{true, true, true, true, false, true, false, true, false, false, false}
+
+	flagtest := []struct {
+		val int
+		res []bool
+	}{
+		{val: 10, res: firstprim},
+		{val: 16, res: append(firstprim, []bool{true, false, true, false, false}...)},
+	}
+
+	var res []bool
+	for _, fl := range flagtest {
+		res = findPrimos(fl.val)
+		for i, so := range fl.res {
+			if len(res) < i && so != res[i] {
+				t.Errorf("Error. value:%d | Expected:%v Actual:%v", fl.val, fl.res, res)
+			}
 		}
 	}
 }
